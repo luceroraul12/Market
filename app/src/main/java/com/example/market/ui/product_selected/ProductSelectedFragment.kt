5b97045b83ca1bdfe253830a4c889cbc3b1b2c8b
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-08 15:58:44 -0300

    Convierto ProductSelected: Activity -> Fragment

---
+++ app/src/main/java/com/example/market/ui/product_selected/ProductSelectedFragment.kt
@@ -0,0 +1,45 @@
+package com.example.market.ui.product_selected
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.market.data.DataMock
+import com.example.market.databinding.FragmentProductBinding
+import com.example.market.databinding.FragmentProductSelectedBinding
+
+class ProductSelectedFragment : Fragment() {
+
+    private val dataMock: DataMock = DataMock()
+
+    private var _binding: FragmentProductSelectedBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val args: ProductSelectedFragmentArgs by navArgs()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProductSelectedBinding.inflate(layoutInflater, container, false)
+        setProductSelected()
+        return binding.root
+    }
+
+    private fun setProductSelected() {
+        val productSelected = dataMock.getById(args.id)
+
+        binding.tvProductSelectedName.text = productSelected.name
+        binding.tvProductSelectedDescription.text = productSelected.description
+        binding.tvProductSelectedUnit.text = productSelected.unit
+        binding.tvProductSelectedPrice.text = productSelected.price.toString()
+    }
+}
