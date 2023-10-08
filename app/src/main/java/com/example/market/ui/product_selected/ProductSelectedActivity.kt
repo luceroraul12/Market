a0c922a87898c29eada44647190b1f59355552fd
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-08 09:27:27 -0300

    Agrego activity para producto seleccionado y configuro un poco el navigation falta que interactuen los botones

---
+++ app/src/main/java/com/example/market/ui/product_selected/ProductSelectedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.market.ui.product_selected
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.market.R
+
+class ProductSelectedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_selected)
+    }
+}
