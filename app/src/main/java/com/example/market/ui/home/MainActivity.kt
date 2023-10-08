f84c5d121ee87843f06de094bb067e650a7a9ae9
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-08 09:06:07 -0300

    agrego botonera de menu y navegador

--- app/src/main/java/com/example/market/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/market/ui/home/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.market.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import com.example.market.R
+import com.example.market.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+
     }
 }
