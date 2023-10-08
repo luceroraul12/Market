da98d788655bebf4696342b306219eba33a720fb
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-08 09:20:31 -0300

    Configuro la barra de menu, falta navigation

--- app/src/main/java/com/example/market/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/market/ui/home/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.market.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.market.R
 import com.example.market.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initNavigation() {
-
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
