1db18138f444cc1bd1babe5a9286043974b6a2fb
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-10 08:31:43 -0300

    configuro binding

--- app/src/main/java/com/example/market/ui/shopping_cart/ShoppingCartFragment.kt
+++ app/src/main/java/com/example/market/ui/shopping_cart/ShoppingCartFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.market.R
+import com.example.market.data.DataMockViewModel
+import com.example.market.databinding.FragmentProductSelectedBinding
+import com.example.market.databinding.FragmentShoppingCartBinding
 import javax.inject.Inject
 
 class ShoppingCartFragment @Inject constructor(): Fragment() {
+
+    private val dataMockViewModel by viewModels<DataMockViewModel>()
+
+    private var _binding: FragmentShoppingCartBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +28,8 @@ class ShoppingCartFragment @Inject constructor(): Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_cart, container, false)
+        _binding = FragmentShoppingCartBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
