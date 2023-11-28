89c4a2b06bc956eb6dd04ced9a83bb184069f593
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-11-28 07:51:18 -0300

    Agrego fragment para historial de ordenes

---
+++ app/src/main/java/com/example/market/ui/orders/OrderFragment.kt
@@ -0,0 +1,28 @@
+package com.example.market.ui.orders
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.market.R
+
+class OrderFragment : Fragment() {
+
+    private lateinit var viewModel: OrderViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_order, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/market/ui/orders/OrderViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.market.ui.orders
+
+import androidx.lifecycle.ViewModel
+
+class OrderViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
