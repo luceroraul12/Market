57ea228764c753ec33941489993e9a2c6f87c183
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-11-28 08:05:57 -0300

    Agrego llamado de para las ordenes

--- app/src/main/java/com/example/market/ui/orders/OrderFragment.kt
+++ app/src/main/java/com/example/market/ui/orders/OrderFragment.kt
@@ -2,27 +2,51 @@ package com.example.market.ui.orders
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.market.R
+import com.example.market.data.DataProductViewModel
+import com.example.market.database.responses.OrderResponse
+import com.example.market.databinding.FragmentOrderBinding
+import com.example.market.databinding.FragmentProductSelectedBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
 
 class OrderFragment : Fragment() {
 
-    private lateinit var viewModel: OrderViewModel
+    private val viewModel by viewModels<OrderViewModel>()
+
+    private var _binding: FragmentOrderBinding? = null
+
+    private lateinit var orders: List<OrderResponse>
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_order, container, false)
+        _binding = FragmentOrderBinding.inflate(layoutInflater, container, false)
+        requestOrders()
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun requestOrders() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                orders = viewModel.getMyOrders("PASIONARIA", "ola@gmail.com")
+                Log.i("ordenes", orders.toString())
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/market/ui/orders/OrderViewModel.kt
+++ app/src/main/java/com/example/market/ui/orders/OrderViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.market.ui.orders
 
 import androidx.lifecycle.ViewModel
+import com.example.market.database.repository.NetworkRepository
+import com.example.market.database.responses.OrderResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class OrderViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class OrderViewModel  @Inject constructor(
+    private val networkRepository: NetworkRepository
+): ViewModel(){
+    suspend fun getMyOrders(storeCode: String, username: String): List<OrderResponse> {
+        return networkRepository.getMyOrders(username, storeCode)
+    }
 }
